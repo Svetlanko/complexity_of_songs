7c9cd6200e24eebc30e24234f1b0fce6e0366cb7
Svetlanko <svetlanko.ivanova@gmail.com>
2016-09-28 11:53:49 -0700

    Make it print the song with minimum logic

---
+++ food_chain.rb
@@ -0,0 +1,79 @@
+require 'pry'
+
+class Song
+  
+  ANIMALS = %w(fly spider bird cat dog goat cow horse)
+
+  def lyrics
+  	text = []
+
+  	ANIMALS.map do |animal|
+  	  text << first_row % animal
+  	  	
+  	  case animal
+  	  when 'fly'
+  	  	text << chorus
+  	  when 'spider'
+  	  	text << 'It wriggled and jiggled and tickled inside her.'
+  	  	text << 'She swallowed the %s to catch the fly.' % animal
+  	  	text << chorus
+  	  when 'bird'
+  	  	text << 'How absurd to swallow a %s!' % animal
+  	  	text << 'She swallowed the %s to catch the spider that wriggled and jiggled and tickled inside her.' % animal
+  	    text << 'She swallowed the spider to catch the fly.'
+  	    text << chorus
+  	  when 'cat'
+  	  	text << 'Imagine that, to swallow a %s!' % animal
+  	    text << 'She swallowed the %s to catch the bird.' % animal
+  	    text << 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.'
+  	    text << 'She swallowed the spider to catch the fly.'
+  	    text << chorus
+  	  when 'dog'
+  	  	text << 'What a hog, to swallow a %s!' % animal
+  	  	text << 'She swallowed the %s to catch the cat.' % animal
+  	  	text << 'She swallowed the cat to catch the bird.'
+        text << 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.'
+        text << 'She swallowed the spider to catch the fly.'
+        text << chorus
+  	  when 'goat'
+  	  	text << 'Just opened her throat and swallowed a %s!' % animal
+  	  	text << 'She swallowed the %s to catch the dog.' % animal
+  	  	text << 'She swallowed the dog to catch the cat.'
+        text << 'She swallowed the cat to catch the bird.'
+        text << 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.'
+        text <<'She swallowed the spider to catch the fly.'
+        text << chorus
+  	  when 'cow' 
+  	  	text << 'I don\'t know how she swallowed a animal!' % animal
+  	  	text << 'She swallowed the %s to catch the goat.' % animal
+        text << 'She swallowed the goat to catch the dog.'
+        text << 'She swallowed the dog to catch the cat.'
+        text << 'She swallowed the cat to catch the bird.'
+        text << 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.'
+        text << 'She swallowed the spider to catch the fly.'
+        text << chorus
+  	  when 'horse'
+  	  	text << the_end
+  	  end	
+  	end
+  end
+
+  def first_row
+  	"I know an old lady who swallowed a %s."
+  end
+
+  def chorus
+  	"I don't know why she swallowed the fly. Perhaps she'll die."
+  end
+
+  def the_end
+  	'She\'s dead, of course!'
+  end
+
+  def print
+    puts lyrics.join("\n")
+  end
+
+end
+
+Song.new.print
