b5368a62ce124359bdfea032ec0961b591fe160f
Svetlanko <svetlanko.ivanova@gmail.com>
2016-09-28 13:38:26 -0700

    Woohoo, it is just getting better and better! Create recite method to handle duplication. Ignore special case with spider for now

--- food_chain.rb
+++ food_chain.rb
@@ -15,47 +15,45 @@ class Song
   	  	text << chorus
   	  when 'spider'
   	  	text << 'It wriggled and jiggled and tickled inside her.'
-        text << swallowed('spider', 'fly')
+  	  	text << recite(animal)
   	  	text << chorus
   	  when 'bird'
   	  	text << 'How absurd to swallow a %s!' % animal
-        text << swallowed('bird', 'spider that wriggled and jiggled and tickled inside her.')
-        text << swallowed('spider', 'fly')
+  	  	text << recite(animal)
   	    text << chorus
   	  when 'cat'
   	  	text << 'Imagine that, to swallow a %s!' % animal
-        text << swallowed('cat', 'bird')
-        text << swallowed('bird', 'spider that wriggled and jiggled and tickled inside her.')
-        text << swallowed('spider', 'fly')
+  	  	text << recite(animal)
   	    text << chorus
   	  when 'dog'
   	  	text << 'What a hog, to swallow a %s!' % animal
-        text << swallowed('dog', 'cat')
-        text << swallowed('cat', 'bird')
-        text << swallowed('bird', 'spider that wriggled and jiggled and tickled inside her.')
-        text << swallowed('spider', 'fly')
+  	  	text << recite(animal)
         text << chorus
   	  when 'goat'
   	  	text << 'Just opened her throat and swallowed a %s!' % animal
-        text << swallowed('goat', 'dog')
-        text << swallowed('dog', 'cat')
-        text << swallowed('cat', 'bird')
-        text << swallowed('bird', 'spider that wriggled and jiggled and tickled inside her.')
-        text << swallowed('spider', 'fly')
+  	  	text << recite(animal)
         text << chorus
   	  when 'cow' 
   	  	text << 'I don\'t know how she swallowed a animal!' % animal
-  	  	text << swallowed('cow', 'goat')
-        text << swallowed('goat', 'dog')
-        text << swallowed('dog', 'cat')
-        text << swallowed('cat', 'bird')
-        text << swallowed('bird', 'spider that wriggled and jiggled and tickled inside her.')
-        text << swallowed('spider', 'fly')
+  	  	text << recite(animal)
         text << chorus
   	  when 'horse'
   	  	text << the_end
-  	  end	
+  	  end
   	end
+  end
+
+  def recite(animal)
+    animal_pairs = swallowed_order animal
+    animal_pairs.map do |pair|
+      swallowed(*pair) unless pair.any?(&:nil?)
+    end
+  end
+
+  def swallowed_order(animal)
+    animals = ANIMALS.reverse
+  	swallowed_animals = animals[animals.index(animal)..-1]
+  	swallowed_animals.zip(swallowed_animals[1..-1]).reject { |pair| pair.include? nil }
   end
 
   def swallowed(animal1, animal2)
@@ -67,7 +65,7 @@ class Song
   end
 
   def chorus
-  	"I don't know why she swallowed the fly. Perhaps she'll die."
+  	"I don't know why she swallowed the fly. Perhaps she'll die.\n"
   end
 
   def the_end
